e43f2cab7591537514ce7cc763bd51048ae48a53
Sumit Kumar <sumk@americana-food.com>
2025-06-22 10:40:32 +0530

    01_variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1440394
+let accountEmail = "sumit@gmail.com"
+var accountPassword = "1234abcd"
+accountCity = "Delhi" 
+let accountState;   //it will give out put undefined, because when varaiable will declared but not initialized it will be undefined.
+
+// accountId = 2  // not allowed, const cannot be reassigned
+
+accountEmail = "sm@gmail.com",
+accountPassword = "abcd1234"
+accountCity = "Mumbai"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
