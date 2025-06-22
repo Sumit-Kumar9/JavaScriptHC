075277a4b3a7c3d5fee292dd734a2bab142c6768
Sumit Kumar <sumk@americana-food.com>
2025-06-22 11:11:32 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";  // treat all JS code as newer version of JS
+
+//number => 2 to power 53
+//bigInt 
+//string= ""
+//boolean => true/false
+//null => standalone value
+//symbol => unique and immutable value
+//object
+
+// Different between undefined and null
+// null = is a standalone value it represents the empity value whereas undefined is a variable that has been declared but not initialized.
+// ex :- let state; //undefined
+// let state = null; //null
+
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); //object
